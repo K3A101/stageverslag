179e707fbb512bd87dd1599da15f27aa3e21a603
K3A101 <keishaalexander2001@outlook.com>
2024-01-02 17:21:35 +0100

    slideshow

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -1,23 +1,44 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
     // Optional parameters
-    autoHeight: true,
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-    centerInsufficientSlides: true,
-    centeredSlidesBounds: true,
+    direction: "horizontal",
     loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 3,
+        slideShadows: true
+    },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 1
+        },
+        992: {
+            slidesPerView: 2
+        },
+        1440: {
+            slidesPerView: 2
+        }
+    },
+
+    //Use arrow keys to navigate slider
+    keyboard: {
+        enabled: true
+    },
 
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
-        activeEl: '.swiper-pagination-bullet-active',
-        clickable: true,
+        el: ".swiper-pagination",
+        clickable: true
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
 });
-
