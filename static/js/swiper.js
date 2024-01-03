ffc534473996cec70421d385531cc38affc1adbf
K3A101 <keishaalexander2001@outlook.com>
2024-01-03 14:17:30 +0100

    New properties for the slideshow

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -1,41 +1,17 @@
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
     direction: "horizontal",
     loop: true,
     centeredSlides: true,
     grabCursor: true,
-    effect: "coverflow",
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 3,
-        slideShadows: true
-    },
+    spaceBetween: 15,
+    slidesPerView: 1.5,
 
-    breakpoints: {
-        576: {
-            slidesPerView: 1
-        },
-        992: {
-            slidesPerView: 2
-        },
-        1440: {
-            slidesPerView: 2
-        }
-    },
 
     //Use arrow keys to navigate slider
     keyboard: {
         enabled: true
     },
-
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
-
+  
     // Navigation arrows
     navigation: {
         nextEl: ".swiper-button-next",
